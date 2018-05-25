b65ff692521fdc0f2d8a137e9d45144e1e27eb46
gdomod <guidohartig@googlemail.com>
2018-05-25 13:37:15 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -22,5 +22,5 @@ masternode=1\n\
 masternodeaddr=$IP:$PORT" >> /root/config.conf
 fi
 
-/usr/local/bin/ximantd -conf=/root/config.conf -masternodeprivkey=$1
+/usr/local/bin/bitcoind -conf=/root/config.conf -masternodeprivkey=$1
 
